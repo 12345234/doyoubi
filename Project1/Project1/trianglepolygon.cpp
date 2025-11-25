75f22962c2b5d8773c02acf9825d77510dcaaecf
DESKTOP-R9SEKJG\a123q <ktc24a31a0066@edu.kyoto-tech.ac.jp>
2025-11-25 17:49:10 +0900

    henkou

--- Project1/Project1/trianglepolygon.cpp
+++ Project1/Project1/trianglepolygon.cpp
@@ -110,10 +110,55 @@ trianglepolygon::~trianglepolygon()
 	return true;
 }
 
+//インデックスバッファ
 [[nodiscard]] bool trianglepolygon::createindexbuffer(const device& device)noexcept
 {
 	uint16_t triangleindeces[] = {
 		0,1,2
 	};
+
+	const auto indexbuffersize = sizeof(triangleindeces);
+
+	D3D12_HEAP_PROPERTIES heapproperty{};
+	heapproperty.Type = D3D12_HEAP_TYPE_UPLOAD;
+	heapproperty.CPUPageProperty = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+	heapproperty.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+	heapproperty.CreationNodeMask = 1;
+	heapproperty.VisibleNodeMask = 1;
+
+
+	D3D12_RESOURCE_DESC resourcedesc{};
+	resourcedesc.Dimension = D3D12_RESOURCE_DIMENSION_BUFFER;
+	resourcedesc.Alignment = 0;
+	resourcedesc.Width = indexbuffersize;
+	resourcedesc.Height = 1;
+	resourcedesc.DepthOrArraySize = 1;
+	resourcedesc.MipLevels = 1;
+	resourcedesc.Format = DXGI_FORMAT_UNKNOWN;
+	resourcedesc.SampleDesc.Count = 1;
+	resourcedesc.SampleDesc.Quality = 0;
+	resourcedesc.Layout =D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+	resourcedesc.Flags = D3D12_RESOURCE_FLAG_NONE;
+
+	auto res = device.get()->CreateCommittedResource(
+		&heapproperty,
+		D3D12_HEAP_FLAG_NONE,
+		&resourcedesc,
+		D3D12_RESOURCE_STATE_DEPTH_READ,
+		nullptr,
+		IID_PPV_ARGS(&indexBuffer));
+	if (FAILED(res))
+	{
+		assert(false && "インデックスバッファの作成に失敗");
+		return false;
+	}
+
+	uint16_t* data{};
+	res = indexBuffer->Map(0, nullptr, reinterpret_cast<void**>(&data));
+	if (FAILED(res))
+	{
+		assert(false&&"インデックスバッファのマップに失敗");
+		return false;
+	}
 
 }
